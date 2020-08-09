6c3f5193abbbd2aeda4e791203996f2f3db2f4dc
Harry <harrybest800@gmail.com>
2020-08-09 11:36:27 +0100

    Simple ping implementation

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+class Shokushu(discord.Client):
+
+    async def on_ready(self):
+        print("logged on as", self.user)
+
+    async def on_message(self, message):
+
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith('8ping'):
+            await message.channel.send('pong')
+
+
+s = Shokushu()
+token = str(os.environ['ShokushuToken'])
+print(token)
+s.run(token)
